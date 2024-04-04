9284b98dd975704175f6ac34b46304276706d376
AD1t12407 <sirigineediaditi@gmail.com>
2024-04-04 07:39:42 +0530

    Added BDA folder- 2 java pgms

---
+++ BDA/WORD_COUNT/BDAWordCountSampleMapper.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class BDAWordCountSampleMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] words = value.toString().split("\\s+");
+        for (String word : words) {
+            context.write(new Text(word), new IntWritable(1));
+        }
+    }
+}
+
